b3191af5c337946280a75290d7aaf6d7a7cc17ab
Hamidul-1 <hamidul786cse@gmail.com>
2022-03-26 23:34:27 +0600

    first commit

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h3>Hoe React Works?</h3>
-            <p>React is a JavaScript library which creats user interface with efficient way using declarative code.We can build single page application or mobile apps by using React.React implements virtual Dom three representation in javaScript.So </p>
+        <div className='style'>
+            <h3>How React Works?</h3>
+            <p>React is a JavaScript library that creates a user interface inefficiently way using declarative code. We can build single-page applications or mobile apps by using React. React implements virtual Dom three representation in javaScript.So when it needs to read or write to the DOM, it Will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser's DOM. </p>
             <h3>Difference between Props vs Stare?</h3>
-            <p>by</p>
+            <p>The props and the state decide what data the component will display. Props are states are re-render the component every time they change. Initially, the props and the state can have default values from a parent component.The major difference is we can not change or modify props but state changeable. Props only readable other hands state readable and writeable.</p>
         </div>
     );
 };
