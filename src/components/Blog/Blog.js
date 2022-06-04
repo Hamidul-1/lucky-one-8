94e4ea976c85bf27e0e63276f7aa75bc9bcd3485
Hamidul-1 <hamidul786cse@gmail.com>
2022-06-04 14:16:10 +0600

    added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,6 +3,7 @@ import './Blog.css'
 
 const Blog = () => {
     return (
+        // blogs
         <div className='style'>
             <h3>How React Works?</h3>
             <p>React is a JavaScript library that creates a user interface inefficiently way using declarative code. We can build single-page applications or mobile apps by using React. React implements virtual Dom three representation in javaScript.So when it needs to read or write to the DOM, it Will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser's DOM. </p>
